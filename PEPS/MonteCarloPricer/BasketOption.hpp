873361f3d8780ce721fef7377b1e2211cf1b75f1
Paul Dudnic <paul.dudnic@grenoble-inp.org>
2021-01-14 17:12:41 +0100

    MonteCarloPricer : Payoffs

---
+++ PEPS/MonteCarloPricer/BasketOption.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Option.hpp"
+
+/// <summary>
+/// Class representing a Basket Option
+/// </summary>
+class BasketOption : public Option
+{
+	public:
+		double strike_; /// Option strike
+
+		virtual double payoff(const PnlMat* path) const;
+
+		/// <summary>
+		/// Creates a Basket Option
+		/// </summary>
+		/// <param name="T">Option maturity</param>
+		/// <param name="nbTimeSteps">Number of discretization steps</param>
+		/// <param name="size">size of the model</param>
+		/// <param name="strike">Option strike</param>
+		BasketOption(double T, int nbTimeSteps, int size, const PnlVect* weighs, double strike);
+
+		~BasketOption();
+};
+
--- PEPS/MonteCarloPricer/Option.cpp
+++ PEPS/MonteCarloPricer/Option.cpp
@@ -1,8 +1,14 @@
 #include "Option.hpp"
 
-Option::Option(double T, int nbTimeSteps, int size)
+Option::Option(double T, int nbTimeSteps, int size, const PnlVect* weights) : 
+	T_(T), 
+	nbTimeSteps_(nbTimeSteps), 
+	size_(size) 
 {
-	T_ = T;
-	nbTimeSteps_ = nbTimeSteps;
-	size_ = size;
+	assetWeights_ = pnl_vect_copy(weights);
+}
+
+Option::~Option()
+{
+	pnl_vect_free(&assetWeights_);
 }
