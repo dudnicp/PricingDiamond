bc4c8a16e29a823c3b7a60d64f1f4ffc5a368a92
Paul Dudnic <paul.dudnic@grenoble-inp.org>
2021-01-14 15:41:13 +0100

    added project for testing the MonteCarlo pricer

---
+++ PEPS/PricerTest/HedgingResults.cpp
@@ -0,0 +1,8 @@
+#include "HedgingResults.hpp"
+#include <iostream>
+
+std::ostream& operator<<(std::ostream& stm, const HedgingResults& res)
+{
+    stm << '{' << "\"initialPrice\": " << res.initialPrice << ", \"initialPriceStdDev\": " << res.initialPriceStdDev << ", \"finalPnL\": " << res.finalPnL << '}';
+    return stm;
+}
