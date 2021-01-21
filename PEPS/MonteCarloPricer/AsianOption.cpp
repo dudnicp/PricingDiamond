a0d7ba03e347eba29e919e674061a22755b682a6
valerianthomas26 <50824540+valerianthomas26@users.noreply.github.com>
2021-01-21 02:15:08 +0100

    Optim + hedge tt t

--- PEPS/MonteCarloPricer/AsianOption.cpp
+++ PEPS/MonteCarloPricer/AsianOption.cpp
@@ -3,17 +3,24 @@
 
 double AsianOption::payoff(const PnlMat* path) const
 {
-	PnlVect* aux = pnl_vect_create(size_);
-	pnl_mat_sum_vect(aux, path, 'r');
-	pnl_vect_div_scalar(aux, nbTimeSteps_ + 1.0);
-	double payoff = pnl_vect_scalar_prod(aux, assetWeights_) - strike_;
+	pnl_mat_sum_vect(aux_, path, 'r');
+	pnl_vect_div_scalar(aux_, nbTimeSteps_ + 1.0);
+	double payoff = pnl_vect_scalar_prod(aux_, assetWeights_) - strike_;
 	return std::max(payoff, 0.0);
 }
 
 AsianOption::AsianOption(double T, int nbTimeSteps, int size, const PnlVect* weights, double strike) :
 	Option(T, nbTimeSteps, size, weights),
 	strike_(strike)
-{}
+{
+	aux_ = pnl_vect_create(size_);
+}
+
+AsianOption::AsianOption(const AsianOption& other) : Option(other)
+{
+	aux_ = pnl_vect_copy(other.aux_);
+	strike_ = other.strike_;
+}
 
 AsianOption* AsianOption::clone() const
 {
@@ -21,4 +28,6 @@ AsianOption* AsianOption::clone() const
 }
 
 AsianOption::~AsianOption()
-{}
+{
+	pnl_vect_free(&aux_);
+}
--- PEPS/MonteCarloPricer/MonteCarlo.cpp
+++ PEPS/MonteCarloPricer/MonteCarlo.cpp
@@ -5,11 +5,11 @@ void MonteCarlo::price(double& estimatedPrice, double& std_dev)
 	double payoffSum = 0.0;
 	double payoffSquaredSum = 0.0;
 	double payoff;
-	PnlMat* assetPath = pnl_mat_create(opt_->nbTimeSteps_ + 1, opt_->size_);
+	
 	for (int i = 0; i < nbSamples_; i++)
 	{
-		mod_->asset(assetPath, opt_->T_, opt_->nbTimeSteps_, rng_);
-		payoff = opt_->payoff(assetPath);
+		mod_->asset(assetPath_, opt_->T_, opt_->nbTimeSteps_, rng_);
+		payoff = opt_->payoff(assetPath_);
 		payoffSum += payoff;
 		payoffSquaredSum += std::pow(payoff, 2);
 	}
@@ -18,8 +18,6 @@ void MonteCarlo::price(double& estimatedPrice, double& std_dev)
 	double payoffMean = payoffSum / nbSamples_;
 	estimatedPrice = discount * payoffMean;
 	std_dev = std::sqrt(std::pow(discount, 2)/nbSamples_ * (payoffSquaredSum / nbSamples_ - std::pow(payoffMean, 2)));
-
-	pnl_mat_free(&assetPath);
 }
 
 void MonteCarlo::price(const PnlMat* past, double t, double& estimatedPrice, double& std_dev)
@@ -27,11 +25,10 @@ void MonteCarlo::price(const PnlMat* past, double t, double& estimatedPrice, dou
 	double payoffSum = 0.0;
 	double payoffSquaredSum = 0.0;
 	double payoff = 0.0;
-	PnlMat* assetPath = pnl_mat_create(opt_->nbTimeSteps_ + 1, opt_->size_);
 	for (int i = 0; i < nbSamples_; i++)
 	{
-		mod_->asset(assetPath, t, opt_->T_, opt_->nbTimeSteps_, rng_, past);
-		payoff = opt_->payoff(assetPath);
+		mod_->asset(assetPath_, t, opt_->T_, opt_->nbTimeSteps_, rng_, past);
+		payoff = opt_->payoff(assetPath_);
 		payoffSum += payoff;
 		payoffSquaredSum += std::pow(payoff, 2);
 	}
@@ -40,14 +37,11 @@ void MonteCarlo::price(const PnlMat* past, double t, double& estimatedPrice, dou
 	double payoffMean = payoffSum / nbSamples_;
 	estimatedPrice = discount * payoffMean;
 	std_dev = std::sqrt(std::pow(discount, 2) / nbSamples_ * (payoffSquaredSum / nbSamples_ - std::pow(payoffMean, 2)));
-
-	pnl_mat_free(&assetPath);
 }
 
 void MonteCarlo::delta(PnlVect* delta, PnlVect* std_dev)
 {
-	PnlMat* assetPath = pnl_mat_create(opt_->nbTimeSteps_ + 1, opt_->size_);
-	PnlMat* shiftedPath = pnl_mat_create(opt_->nbTimeSteps_ + 1, opt_->size_);
+
 	double timestep = opt_->T_ / (opt_->nbTimeSteps_);
 	int d, i;
 	double payoffShiftPlusH, payoffShiftMinusH;
@@ -56,28 +50,28 @@ void MonteCarlo::delta(PnlVect* delta, PnlVect* std_dev)
 
 	for (i = 0; i < nbSamples_; i++)
 	{
-		mod_->asset(assetPath, opt_->T_, opt_->nbTimeSteps_, rng_);
-		pnl_mat_clone(shiftedPath, assetPath);
+		mod_->asset(assetPath_, opt_->T_, opt_->nbTimeSteps_, rng_);
+		pnl_mat_clone(shiftedPath_, assetPath_);
 		for (d = 0; d < opt_->size_; d++)
 		{
-			mod_->shiftAsset(shiftedPath, assetPath, d, fdStep_, 0);
-			payoffShiftPlusH = opt_->payoff(shiftedPath);
-			mod_->shiftAsset(shiftedPath, assetPath, d, -fdStep_, 0);
-			payoffShiftMinusH = opt_->payoff(shiftedPath);
-			mod_->shiftAsset(shiftedPath, assetPath, d, 0, 0);
+			mod_->shiftAsset(shiftedPath_, assetPath_, d, fdStep_, 0);
+			payoffShiftPlusH = opt_->payoff(shiftedPath_);
+			mod_->shiftAsset(shiftedPath_, assetPath_, d, -fdStep_, 0);
+			payoffShiftMinusH = opt_->payoff(shiftedPath_);
+			mod_->shiftAsset(shiftedPath_, assetPath_, d, 0, 0);
 			pnl_vect_set(delta, d, pnl_vect_get(delta, d) + payoffShiftPlusH - payoffShiftMinusH);
 			pnl_vect_set(std_dev, d, pnl_vect_get(std_dev, d) + std::pow(payoffShiftPlusH - payoffShiftMinusH, 2));
 		}
 	}
-	
+
 	double aux = std::exp(-mod_->r_ * opt_->T_) / (2 * fdStep_);
 
 	// Delta calculation
-	pnl_vect_mult_scalar(delta, aux/nbSamples_);
+	pnl_vect_mult_scalar(delta, aux / nbSamples_);
 	pnl_vect_div_vect_term(delta, mod_->spot_);
 
 	// delta std_dev calculation
-	pnl_vect_mult_scalar(std_dev, std::pow(aux, 2)/nbSamples_);
+	pnl_vect_mult_scalar(std_dev, std::pow(aux, 2) / nbSamples_);
 	auto f = [](double x, double y)
 	{
 		return x / std::pow(y, 2);
@@ -92,35 +86,28 @@ void MonteCarlo::delta(PnlVect* delta, PnlVect* std_dev)
 
 	pnl_vect_div_scalar(std_dev, nbSamples_);
 	pnl_vect_map_inplace(std_dev, std::sqrt);
-
-	pnl_mat_free(&assetPath);
-	pnl_mat_free(&shiftedPath);
-
 }
 
 void MonteCarlo::delta(const PnlMat* past, double t, PnlVect* delta, PnlVect* std_dev)
 {
-	PnlMat* assetPath = pnl_mat_create(opt_->nbTimeSteps_ + 1, opt_->size_);
-	PnlMat* shiftedPath = pnl_mat_create(opt_->nbTimeSteps_ + 1, opt_->size_);
 	double timestep = (double)(opt_->T_ / (opt_->nbTimeSteps_));
-
 	double payoffShiftPlusH, payoffShiftMinusH;
 	pnl_vect_set_zero(delta);
 	pnl_vect_set_zero(std_dev);
 	int d;
-	int shiftIndex = (int)std::ceil(t / timestep);
+ 	int shiftIndex = (int)std::ceil(t / timestep);
 
 	for (int i = 0; i < nbSamples_; i++)
 	{
-		mod_->asset(assetPath, t, opt_->T_, opt_->nbTimeSteps_, rng_, past);
-		pnl_mat_clone(shiftedPath, assetPath);
+		mod_->asset(assetPath_, t, opt_->T_, opt_->nbTimeSteps_, rng_, past);
+		pnl_mat_clone(shiftedPath_, assetPath_);
 		for (d = 0; d < opt_->size_; d++)
 		{
-			mod_->shiftAsset(shiftedPath, assetPath, d, fdStep_, shiftIndex);
-			payoffShiftPlusH = opt_->payoff(shiftedPath);
-			mod_->shiftAsset(shiftedPath, assetPath, d, -fdStep_, shiftIndex);
-			payoffShiftMinusH = opt_->payoff(shiftedPath);
-			mod_->shiftAsset(shiftedPath, assetPath, d, 0, shiftIndex);
+			mod_->shiftAsset(shiftedPath_, assetPath_, d, fdStep_, shiftIndex);
+			payoffShiftPlusH = opt_->payoff(shiftedPath_);
+			mod_->shiftAsset(shiftedPath_, assetPath_, d, -fdStep_, shiftIndex);
+			payoffShiftMinusH = opt_->payoff(shiftedPath_);
+			mod_->shiftAsset(shiftedPath_, assetPath_, d, 0, shiftIndex);
 			pnl_vect_set(delta, d, pnl_vect_get(delta, d) + payoffShiftPlusH - payoffShiftMinusH);
 			pnl_vect_set(std_dev, d, pnl_vect_get(std_dev, d) + std::pow(payoffShiftPlusH - payoffShiftMinusH, 2));
 		}
@@ -146,9 +133,6 @@ void MonteCarlo::delta(const PnlMat* past, double t, PnlVect* delta, PnlVect* st
 	pnl_vect_map_vect_inplace(std_dev, delta, g);
 	pnl_vect_div_scalar(std_dev, nbSamples_);
 	pnl_vect_map_inplace(std_dev, std::sqrt);
-
-	pnl_mat_free(&assetPath);
-	pnl_mat_free(&shiftedPath);
 }
 
 MonteCarlo::MonteCarlo(const BlackScholesModel* mod, const Option* opt, PnlRng* rng, double fdStep, int nbSamples)
@@ -158,6 +142,8 @@ MonteCarlo::MonteCarlo(const BlackScholesModel* mod, const Option* opt, PnlRng* 
 	rng_ = pnl_rng_copy(rng);
 	fdStep_ = fdStep;
 	nbSamples_ = nbSamples;
+	assetPath_ = pnl_mat_create(opt_->nbTimeSteps_ + 1, opt_->size_);
+	shiftedPath_ = pnl_mat_create(opt_->nbTimeSteps_ + 1, opt_->size_);
 }
 
 MonteCarlo::~MonteCarlo()
@@ -165,61 +151,59 @@ MonteCarlo::~MonteCarlo()
 	delete mod_;
 	delete opt_;
 	pnl_rng_free(&rng_);
+	pnl_mat_free(&shiftedPath_);
+	pnl_mat_free(&assetPath_);
 }
 
 
 double MonteCarlo::profitAndLoss(const PnlMat* marketPath, double T, double N)
 {
 	int size = marketPath->n, timeIter, i = 0;
-	double H = (double)marketPath->m - 1, stepInN = H/N;
+	double H = (double)marketPath->m - 1;
 	double t; // date dans le numéraire N
 	double V = 0, aux = exp(mod_->r_*T/H);
 	double payoff, p0, std_dev_value;
 	price(p0, std_dev_value);
 
 	/*Calcul des deltas sur les H dates*/
-	PnlMat* deltas = pnl_mat_create((int)H+1, size);
 	PnlMat* past = pnl_mat_create((int)N + 1, size);
 	PnlVect* delta_t = pnl_vect_create(size);
+	PnlVect* diffDelta = pnl_vect_create(size);
 	PnlVect* std_dev = pnl_vect_create(size);
 	PnlVect* pathGetter = pnl_vect_create(size);
 	PnlVect* lastDelta = pnl_vect_create(size);
 	for (timeIter = 0; timeIter <= H; timeIter++)
 	{
-		t = timeIter * N / H;
+		std::cout << timeIter << std::endl;
+		t = timeIter * T / H ;
 		/*On remplit avec la valeur en t, les valeurs précédentes étant déja enregistrées*/
 		pnl_mat_get_row(pathGetter, marketPath, timeIter);
-		pnl_mat_set_row(past, pathGetter, (int)floor(t));
-
-		pnl_vect_print(pathGetter);
+		pnl_mat_set_row(past, pathGetter, (int)ceil(timeIter*N/H));
 
 		/*On apelle la fonction delta pour remplir le vecteur*/
-		this->delta(past, t, delta_t, std_dev);
-		pnl_mat_set_row(deltas, delta_t, timeIter);
-
-		pnl_vect_print(delta_t);
-
+		delta(past, t, delta_t, std_dev);
 		if (timeIter == 0)
 		{
 			V = p0 - pnl_vect_scalar_prod(delta_t, pathGetter);
-			lastDelta = pnl_vect_copy(delta_t);
+			pnl_vect_clone(lastDelta, delta_t);
 		}
 		else 
 		{
-			pnl_vect_minus_vect(delta_t, lastDelta); // On met dans delta delta-lastdelta
+			pnl_vect_clone(diffDelta, delta_t);
+			pnl_vect_minus_vect(diffDelta, lastDelta);
 			V *= aux;
-			V -= pnl_vect_scalar_prod(delta_t, pathGetter);
-			pnl_mat_get_row(lastDelta, deltas, timeIter); // On met dans lastDelta le dernier vecteur de delta calculé
+			V -= pnl_vect_scalar_prod(diffDelta, pathGetter);
+			pnl_vect_clone(lastDelta, delta_t);
 		}
 	}
 	payoff = opt_->payoff(past);
-
-	pnl_mat_free(&deltas);
+	std_dev_value = pnl_vect_scalar_prod(lastDelta, pathGetter);
 	pnl_mat_free(&past);
+	pnl_vect_free(&diffDelta);
 	pnl_vect_free(&delta_t);
 	pnl_vect_free(&std_dev);
 	pnl_vect_free(&pathGetter);
 	pnl_vect_free(&lastDelta);
 
-	return V + pnl_vect_scalar_prod(lastDelta, pathGetter) - payoff;
+	return V + std_dev_value - payoff;
 }
