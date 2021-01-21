e703f92cf4797dc9d6ffd07c8063197c185600d3
Léa Angles <lea.angles@grenoble-inp.org>
2021-01-21 17:30:32 +0100

    getPrice et getDelta fait

--- PEPS/Wrapper/Pricer.cpp
+++ PEPS/Wrapper/Pricer.cpp
@@ -1,2 +1,59 @@
 #include "pch.h"
+
 #include "Pricer.hpp"
+
+
+using namespace MonteCarloPricer;
+namespace Pricer {
+	PricerClass::PricerClass(PnlMat* simulatedMarketData, PnlVect* observationDates, PnlVect* currentSpots, PnlVect* changeRates) {
+		//TODO
+	}
+
+	void PricerClass::getPrice(double date) {
+		int nbTimeSteps = observationDates_->size;
+		int size = currentSpots_->size;
+
+		size_t t = 0;
+		while (t < nbTimeSteps && pnl_vect_get(this->observationDates_, t) < date) {
+			t++;
+		}
+
+		PnlMat* past = pnl_mat_create(nbTimeSteps + 1, size);
+
+		for (size_t i = 0; i < t; i++)
+		{
+			for (size_t j = 0; j < size; j++)
+			{
+				pnl_mat_set(past, i, j, pnl_mat_get(this->simulatedMarketData_, i, j));
+			}
+		}
+
+
+		this->monteCarlo->price(past, date, this->price, this->std_dev);
+	}
+
+	void PricerClass::getDelta(double date) {
+		int nbTimeSteps = observationDates_->size;
+		int size = currentSpots_->size;
+
+		size_t t = 0;
+		while (t < nbTimeSteps && pnl_vect_get(this->observationDates_, t) < date) {
+			t++;
+		}
+
+		PnlMat* past = pnl_mat_create(nbTimeSteps + 1, size);
+
+		for (size_t i = 0; i < t; i++)
+		{
+			for (size_t j = 0; j < size; j++)
+			{
+				pnl_mat_set(past, i, j, pnl_mat_get(this->simulatedMarketData_, i, j));
+			}
+		}
+
+
+		this->monteCarlo->delta(past, date, this->delta, this->std_dev_vect);
+
+	}
+}
+
--- PEPS/Wrapper/Pricer.hpp
+++ PEPS/Wrapper/Pricer.hpp
@@ -4,12 +4,27 @@
 #include "pnl/pnl_vector.h"
 #include "pnl/pnl_random.h"
 
-public ref class Pricer
-{
+#include "MonteCarlo.hpp"
+
+namespace Pricer {
+
+	public ref class PricerClass
+	{
 	private:
+		MonteCarlo* monteCarlo;
 		PnlMat* simulatedMarketData_;
 		PnlVect* observationDates_;
 		PnlVect* currentSpots_;
 		PnlVect* changeRates_;
-};
 
+		double& price;
+		double& std_dev;
+		PnlVect* delta;
+		PnlVect* std_dev_vect;
+
+	public:
+		PricerClass(PnlMat* simulatedMarketData, PnlVect* observationDates, PnlVect* currentSpots, PnlVect* changeRates);
+		void getPrice(double date);
+		void getDelta(double date);
+	};
+}
