99e540bc27dfb6334d94819f3dfaaba3aba75c69
Paul Dudnic <paul.dudnic@grenoble-inp.org>
2021-01-21 06:16:28 +0100

    PEPS : ViewModel + Bindings done, minor tweaks to ui

--- PEPS/PEPS/Model/Asset.cs
+++ PEPS/PEPS/Model/Asset.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,45 @@ namespace PEPS.Model
     /// <summary>
     /// Class representing a financial asset
     /// </summary>
-    public abstract class Asset
+    public abstract class Asset : INotifyPropertyChanged
+
     {
+        private string _name;
+        private double _euroPrice;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         /// <summary>
         /// Name of the asset
         /// </summary>
-        public string Name { get; protected set; }
+        public string Name
+        {
+            get => _name;
+            protected set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
 
         /// <summary>
         /// Price of the asset in euros
         /// </summary>
-        public double EuroPrice { get; protected set; }
+        public double EuroPrice
+        {
+            get => _euroPrice;
+            protected set
+            {
+                if (value != _euroPrice)
+                {
+                    _euroPrice = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("EuroPrice"));
+                }
+            }
+        }
 
         /// <summary>
         /// Constructor, creates an Asset with the given name
--- PEPS/PEPS/Model/PortfolioComponent.cs
+++ PEPS/PEPS/Model/PortfolioComponent.cs
@@ -3,29 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace PEPS.Model
 {
     /// <summary>
     /// Class represnting a hedging portfolio component
     /// </summary>
-    class PortfolioComponent
+    public class PortfolioComponent : INotifyPropertyChanged
     {
+        private Asset _asset;
+        private double _quantity;
+        private double _totalPrice;
+
         /// <summary>
         /// Portfolio asset
         /// </summary>
-        public Asset Asset { get; protected set; }
+        public Asset Asset
+        {
+            get => _asset;
+            protected set
+            {
+                if (value != _asset)
+                {
+                    _asset = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Asset"));
+                }
+            }
+        }
 
         /// <summary>
         /// Number of assets (of this type) in the portfolio
         /// </summary>
-        public double Quantity { get; protected set; }
+        public double Quantity
+        {
+            get => _quantity;
+            protected set
+            {
+                if (value != _quantity)
+                {
+                    _quantity = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Quantity"));
+                }
+            }
+        }
 
         /// <summary>
         /// Total price of this component in euros (= Asset.EuroPrice * Quantity)
         /// <see cref="Asset.EuroPrice"/>
         /// </summary>
-        public double TotalPrice { get; protected set; }
+        public double TotalPrice
+        {
+            get => _totalPrice;
+            protected set
+            {
+                if (value != _totalPrice)
+                {
+                    _totalPrice = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TotalPrice"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
         /// Constructor, creates a PortfolioComponent with the given Asset and quantity
